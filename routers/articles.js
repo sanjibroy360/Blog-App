9c2fd8e71a69f7bf0e82d23cc323f450ff5661b3
Sanjib Roy <sanjibroy0098@gmail.com>
2020-05-05 22:46:22 +0530

    Modify Code

--- routers/articles.js
+++ routers/articles.js
@@ -4,16 +4,13 @@ const router = express.Router();
 const Article = require('../models/article');
 
 router.get('/:username/article', (req, res, next) => {
-    console.log(req.params.username);
     res.render("articleHome", { username: req.params.username });
 });
-
 
 // Create
 
 router.get('/:username/article/new', (req, res, next) => {
 
-    console.log(req.params.username);
     Article.create(req.body, (err, data) => {
         if (err) {
             return next(err);
